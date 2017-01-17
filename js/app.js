e733110cbb088aca4f31d9a07c3c2bc3ff6a5477
Sam Fentress <sfentress@concord.org>
2017-01-17 16:04:05 -0500

    Add initial controllable melanogenesis animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 var Snap = require("snapsvg");
+import Melanogenesis from "./animations/melanogenesis";
 
-const s = Snap(800, 600);
+const snap = Snap("#game");
+
+const melanogenesis = new Melanogenesis(snap, Melanogenesis.WORKING_TYR1);
+melanogenesis.run();
+
+
+var radios = document.getElementsByTagName("input");
+  for(let radio of radios) {
+    radio.onchange = function() {
+      if (radio.checked) {
+        if (radio.name == "melanogenesis") {
+          melanogenesis.setVersion(Melanogenesis[radio.id]);
+        }
+      }
+    }
+}
