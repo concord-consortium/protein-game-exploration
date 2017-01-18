1d3aaf26b4e3108942c210b4ef549bfd201b71a4
Sam Fentress <sfentress@concord.org>
2017-01-18 14:05:46 -0500

    Refactor into individual folders; req npm install

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
 var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: "./js/app.js",
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "app.js"
-    },
-    module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-              presets: ['es2015']
-            }
-          },
-          {
-            test: /\.css$/,
-            loader: 'style!css!autoprefixer'
-          },
-          {
-            // See https://github.com/adobe-webplatform/Snap.svg/issues/341
-            test: require.resolve('snapsvg'),
-            loader: 'imports-loader?this=>window,fix=>module.exports=0'
-          }
-        ]
+  entry: {
+    'simple-animation': './simple-animation/js/app.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: path.join('[name]', '[name].js')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css!autoprefixer'
+      },
+      {
+        // See https://github.com/adobe-webplatform/Snap.svg/issues/341
+        test: require.resolve('snapsvg'),
+        loader: 'imports-loader?this=>window,fix=>module.exports=0'
       }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: path.join(__dirname, 'simple-animation', 'public'), to: 'simple-animation'}
+    ])
+  ]
 };
