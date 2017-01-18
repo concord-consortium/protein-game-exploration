fb6fd7bd5687dddfad620cee99402166b2cdab06
Sam Fentress <sfentress@concord.org>
2017-01-18 14:58:52 -0500

    Simple fading between drakes

--- simple-animation/js/app.js
+++ simple-animation/js/app.js
@@ -26,7 +26,32 @@ let drakes = document.querySelectorAll("#drake img"),
         el.classList.remove("hidden");
       else
         el.className = el.className.replace(new RegExp('(^|\\b)' + "hidden".split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
+    },
+    fade = (el, fadeIn, callback) => {
+      if (fadeIn) {
+        el.style.opacity = 0;
+      } else {
+        el.style.opacity = 1;
+      }
+
+      var last = +new Date();
+      var tick = function() {
+        if (fadeIn) {
+          el.style.opacity = +el.style.opacity + (new Date() - last) / 5000;
+        } else {
+          el.style.opacity = +el.style.opacity - (new Date() - last) / 5000;
+        }
+        last = +new Date();
+
+        if (fadeIn && +el.style.opacity < 1 || !fadeIn && +el.style.opacity > 0) {
+          (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+        } else if (callback) {
+          callback();
+        }
+      };
+
+      tick();
+    };
 
 var radios = document.getElementsByTagName("input");
   for(let radio of radios) {
@@ -39,10 +64,15 @@ var radios = document.getElementsByTagName("input");
         } else if (radio.name == "gate") {
           gate.setVersion(Gate[radio.id]);
         }
-        hide(drakes);
-        let drakeId = melanogenesis.version+"_"+transport.version+"_"+gate.version,
-            drake = document.getElementById(drakeId);
-        show(drake);
+
+        setTimeout( () => {
+          let visibleDrake = document.querySelectorAll("#drake img:not(.hidden)")[0];
+          let drakeId = melanogenesis.version+"_"+transport.version+"_"+gate.version,
+              drake = document.getElementById(drakeId);
+          show(drake);
+          fade(visibleDrake, false);
+          fade(drake, true, () => {hide(drakes); show(drake);});
+        }, 500);
       }
     }
 }
